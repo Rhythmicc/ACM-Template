a04109deb84d775bad0501c46904a4df7d012de5
连浩丞 <RhythmLian@outlook.com>
2019-08-05 14:37:02 +0800

    None

---
+++ cmake-build-debug/CMakeFiles/ACM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ACM.dir/main.cpp.o"
+  "ACM.pdb"
+  "ACM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ACM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
