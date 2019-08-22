2742322c842c871e5e4735abb1c50016dfea8c7f
连浩丞 <RhythmLian@outlook.com>
2019-08-22 13:36:13 +0800

    add run.sh / run.bat

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ basedir=$(cd "$(dirname "$0")" || exit; pwd -P)
 if [[ $1 = "-br" || $1 = "-b" ]]; then
   g++ -std=c++11 "$basedir"/main.cpp -o "$basedir"/cmake-build-debug/ACM
 fi
-if [[ $1 = "-br" || $1 = "-r" || -z $1 ]]; then
+if [[ $1 != "-b" ]]; then
   "$basedir"/cmake-build-debug/ACM < "$basedir"/cmake-build-debug/input.txt
 fi
