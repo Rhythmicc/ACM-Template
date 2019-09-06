252050f65a0ecd53e4a9c5cb2f1586d6afdc9ab7
连浩丞 <RhythmLian@outlook.com>
2019-09-06 09:00:17 +0800

    update

--- README.md
+++ README.md
@@ -3,12 +3,23 @@
 - 运行脚本:
 
   - **要使用这个脚本，你应该有`g++`环境和`python3`环境**
-  - 编译且运行: `./run.py -br`
-  - 编译不运行: `./run.py -b`
-  - 只 是 运 行 : `./run.py -r` 或 `./run.py`
-  - ( 你可以编辑 `./cmake-build-debug/input.txt` 来设置输入)
-  - ( 你可以运行 `./run.py [-br] > output.txt` 使程序输出到 `./output.txt`)
+  - 编译或运行
   
+      - [ -b ] : 编译
+      - [ -r ] : 运行
+      - [ -br] : 编译且运行
+      - ( 如果上述三个命令都不存在，则默认运行当前编译好的程序 )
+
+  - 输入输出:
+      
+      - 运行 `./run.py [...] -i` 使程序使用`./cmake-build-debug/input.txt`作为输入
+      - 你可以编辑 `./cmake-build-debug/input.txt` 来设置输入
+      - 运行 `./run.py [...] > output.txt` 使程序输出到 `./output.txt`
+      
+  - 推荐的命令示例:
+      - `./run.py -i` : 使用默认输入文件并运行。
+      - `./run.py`: 运行。
+      - `./run.py -i -br` or `./run.py -br -i`: 编译且使用输入文件运行。
 - 刷新脚本:
 
   - 运行 `./refresh.py` 来初始化 `main.cpp` 为存储在 `./template/main` 文件中的内容。
--- run.py
+++ run.py
@@ -10,19 +10,26 @@ else:
 base_dir += dir_char
 
 
-def run():
-    os.system(
-        base_dir + 'cmake-build-debug' + dir_char + 'ACM < ' + base_dir + 'cmake-build-debug' + dir_char + 'input.txt')
+def run(use_txt=False):
+    cmd = base_dir + 'cmake-build-debug' + dir_char + 'ACM' + (
+        ' < ' + base_dir + 'cmake-build-debug' + dir_char + 'input.txt' if use_txt else '')
+    os.system(cmd)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        run()
+    to_build = '-b' in sys.argv or '-br' in sys.argv
+    to_run = '-r' in sys.argv or '-b' not in sys.argv
+    if '-h' in sys.argv:
+        print('usage: run.py:\n'
+              '\tbuild or run:\n'
+              '\t\t[ -b ] : build\n'
+              '\t\t[ -r ] : run\n'
+              '\t\t[ -br] : build and run\n'
+              '\t\t(it will run if neither of commands in "build or run")\n'
+              '\t[ -i ] : use input.txt as input\n'
+              '\t[ -h ] : help')
         exit(0)
-    elif len(sys.argv) != 2:
-        print('usage: run.py [-b / -r / -br]')
-        exit(-1)
-    elif sys.argv[1] == '-br' or sys.argv[1] == '-b':
+    if to_build:
         os.system('g++ -std=c++11 ' + base_dir + 'main.cpp -o ' + base_dir + 'cmake-build-debug' + dir_char + 'ACM')
-    if sys.argv[1] != '-b':
-        run()
+    if to_run:
+        run('-i' in sys.argv)
